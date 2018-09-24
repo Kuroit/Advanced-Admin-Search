b37b3ab2cfa5ac213fe5eca786a1f47637a216da
Priya <priya.jangral851@gmail.com>
2018-09-24 13:31:06 +0530

    Minor update

--- advanced-admin-search.php
+++ advanced-admin-search.php
@@ -1,11 +1,11 @@
 <?php
    /*
    Plugin Name: Advanced Admin Search
-   Plugin URI: https://www.kuroit.com
+   Plugin URI: 
    description: A WordPress plugin which adds extra searching feature into admin bar.
    Version: 0.9
-   Author: Kuroit
-   Author URI: https://www.kuroit.com
+   Author: 
+   Author URI: 
    License: GPLv2 or later
    License URI: http://www.gnu.org/licenses/gpl-2.0.html
    */
@@ -13,7 +13,7 @@
 	*
 	* Advanced Admin Search is a wordpress plugin which adds extra searching feature into admin bar.
 	*
-	**/
+	*/
 namespace Kuroit\AdvancedAdminSearch;
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -24,15 +24,9 @@ class AASKP_advancedAdminSearch{
 
 function __construct() {
 
-<<<<<<< HEAD
 		// Hook to add input box in admin panel for searching.
         add_action('admin_bar_menu', array( $this, 'AASKP_SearchBox'));
         add_action('in_admin_header', array( $this, 'AASKP_displayInput'));
-=======
-	// Hook to add input box in admin panel for searching.
-        add_action('admin_bar_menu', array( $this, 'SearchBox'));
-        add_action('in_admin_header', array( $this, 'displayInput'));
->>>>>>> c60d99dbda118b9ba235437624deb75a9d7e04ac
 
         // Hooks to get the searching data like admin menu, media labraries, post, pages.
         add_action( 'admin_bar_menu', array( $this, 'AASKP_desktopSearchJavascript' ));
@@ -197,11 +191,5 @@ else
 }
 
 }
-<<<<<<< HEAD
 new AASKP_advancedAdminSearch();
 ?>
-=======
-
-new AASK_advancedAdminSearch();
-?>
->>>>>>> c60d99dbda118b9ba235437624deb75a9d7e04ac
